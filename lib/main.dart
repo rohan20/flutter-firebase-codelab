0d9da3739903eca91415ff9a24c12c125ab29f09
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-08-08 17:27:47 +0530

    added cloud firestore dependencies; add multiDex enabled to android gradle; fixed adding GoogleServices-plist.json in iOS using xcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,43 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Baby Names',
+      home: const MyHomePage(title: 'Baby Name Votes'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text(title)),
+      body: new StreamBuilder(
+          stream: Firestore.instance.collection('footballers').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+            return new ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                padding: const EdgeInsets.only(top: 10.0),
+                itemExtent: 25.0,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot ds = snapshot.data.documents[index];
+                  return new Text(" ${ds['name']} ${ds['votes']}");
+                }
+            );
+          }),
+    );
+  }
+}
